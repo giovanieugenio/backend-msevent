c53b75ddbec4b452ed4a9ff02a172d18d38070aa
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-06-20 23:43:08 -0300

    add repositories

---
+++ src/main/java/com/ms/rest_api/repositories/EventRepository.java
@@ -0,0 +1,9 @@
+package com.ms.rest_api.repositories;
+
+import com.ms.rest_api.domain.event.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EventRepository extends JpaRepository<Event, UUID> {
+}
